98ee7904e42d1bf1e312f55e8184b652650d9e8a
Matheus Castelo <matheuscastelo.tech@gmail.com>
2026-04-25 17:57:38 -0300

    add: q2 main.py

---
+++ q2-laboratorio-compartilhado/main.py
@@ -0,0 +1,29 @@
+import time
+from src.utils.constants import NUM_PROGRAMMERS
+from src.services.resource_manager import ResourceManager
+from src.services.display_service import DisplayService
+from src.models.programmer import Programmer
+
+def main():
+    print("Iniciando o trabalho no laboratório...")
+    print("-" * 50)
+
+    resource_manager = ResourceManager()
+    display_service = DisplayService()
+
+    programmers = []
+    for i in range(1, NUM_PROGRAMMERS + 1):
+        programmer = Programmer(i, resource_manager, display_service)
+        programmers.append(programmer)
+
+    for programmer in programmers:
+        programmer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\n\n Dia de trabalho finalizado!.")
+
+if __name__ == "__main__":
+    main()
