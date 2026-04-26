3ab31f8fc4c4b6bbcf529c4ce473878da57fabbb
Breno Brigagão <brenobrigagaodev@gmail.com>
2026-04-26 15:50:11 -0300

    enter starvation

--- q3-laboratorio-compartilhado/src/models/VetRoomStarvation.py
+++ q3-laboratorio-compartilhado/src/models/VetRoomStarvation.py
@@ -7,3 +7,17 @@ class VetRoomStarvation:
         self.cat_lock = threading.Lock()
         self.dog_count = 0
         self.cat_count = 0
+
+    def enter(self,animal_id,species):
+        if species == "DOG":
+            with self.dog_lock:
+                if self.dog_count == 0:
+                    self.door.acquire()
+                self.dog_count += 1
+        else:
+            with self.cat_lock:
+                if self.cat_count == 0:
+                    self.door.acquire()
+                self.cat_count += 1
+        print(f"[{species}] {animal_id} ENTROU na sala.")
+    
